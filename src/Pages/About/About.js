58497fb7690e4d6f39f47d3631b4c6e5f15cc0f4
karuna <kanajijob@gmail.com>
2022-08-01 22:36:40 -0400

    Update about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className = "aboutcontents">
         <h1 className = "toptext">Hi, I'm Karuna!</h1>
         <h1 className = "sectiontitle">About</h1>
-        <p className = "bodypara">I'm a highschool student looking to apply my passion for Computer Science and Engineering to explore new technologies and create solutions for real-world problems.</p>
+        <p className = "bodypara">I'm a first year Engineering student at UofT looking to apply my passion for Computer Science and Engineering to explore new technologies and create solutions for real-world problems.</p>
       </div>
     </div>
   )
